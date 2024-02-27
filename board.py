e78092356ebfaafb75fc70c2f251b4a71b4ea986
Cir0n <philippe.baba3000@gmail.com>
2024-02-27 09:48:34 +0100

    Fonction Validite() marche !

--- board.py
+++ board.py
@@ -8,29 +8,53 @@ def verification(tableau, joueur):
     # Vérifie horizontalement si 4 pions sont aligné
     for i in range(tableau.shape[1] - 3):
         for j in range(tableau.shape[0]):
-            if tableau[j, i] == joueur and tableau[j, i + 1] == joueur and tableau[j, i + 2] == joueur and tableau[j, i + 3] == joueur:
+            if tableau[j, i] == joueur and tableau[j, i + 1] == joueur and tableau[j, i + 2] == joueur and tableau[
+                j, i + 3] == joueur:
                 print(f'joueur {joueur} a gagné')
                 return True
     # Vérifie verticalement si 4 pions sont aligner
     for i in range(tableau.shape[1]):
         for j in range(tableau.shape[0] - 3):
-            if tableau[j, i] == joueur and tableau[j + 1, i] == joueur and tableau[j + 2, i] == joueur and tableau[j + 3, i] == joueur:
+            if tableau[j, i] == joueur and tableau[j + 1, i] == joueur and tableau[j + 2, i] == joueur and tableau[
+                j + 3, i] == joueur:
                 print(f'joueur {joueur} a gagné')
                 return True
     # Vérifie diagonal
     for i in range(tableau.shape[1] - 3):
         for j in range(tableau.shape[0] - 3):
-            if tableau[j, i] == joueur and tableau[j + 1, i + 1] == joueur and tableau[j + 2, i + 2] == joueur and tableau[j + 3, i + 3] == joueur:
+            if tableau[j, i] == joueur and tableau[j + 1, i + 1] == joueur and tableau[j + 2, i + 2] == joueur and \
+                    tableau[j + 3, i + 3] == joueur:
                 print(f'joueur {joueur} a gagné')
                 return True
     # Vérifie autre diagonal
     for i in range(3, tableau.shape[1]):
         for j in range(tableau.shape[0]):
-            if tableau[j, i] == joueur and tableau[j + 1, i - 1] == joueur and tableau[j + 2, i - 2] == joueur and tableau[j + 3, i - 3] == joueur:
+            if tableau[j, i] == joueur and tableau[j + 1, i - 1] == joueur and tableau[j + 2, i - 2] == joueur and \
+                    tableau[j + 3, i - 3] == joueur:
                 print(f'joueur {joueur} a gagné')
                 return True
 
+def validite(tableau,position_jouer):
+    if tableau[0, position_jouer] == 0:
+        return True
+    print('ne peut pas jouer')
+    return False
 
+assert validite(np.array(
+    [[0, 1, 0, 0, 1, 1, 1],
+     [1, 1, 1, 1, 2, 0, 0],
+     [1, 1, 2, 2, 1, 0, 0],
+     [2, 2, 1, 2, 1, 1, 2],
+     [0, 2, 1, 2, 1, 2, 1],
+     [2, 1, 1, 1, 2, 2, 1]]), 1) == False, "Validité erreur"
+
+assert validite(np.array(
+    [[0, 0, 0, 0, 1, 1, 1],
+     [1, 1, 1, 1, 2, 0, 0],
+     [1, 1, 2, 2, 1, 0, 0],
+     [2, 2, 1, 2, 1, 1, 2],
+     [0, 2, 1, 2, 1, 2, 1],
+     [2, 1, 1, 1, 2, 2, 1]]), 1), "Validité erreur"
 
 assert verification(np.array(
     [[0, 1, 0, 0, 1, 1, 1],
