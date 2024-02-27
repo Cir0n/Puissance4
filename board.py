1c3b3b73a485d7960eb0f1c147179d7a9160bbe8
unknown <vdugleux@gamingcampus.local>
2024-02-27 08:31:09 +0100

    correct 1

--- board.py
+++ board.py
@@ -7,17 +7,17 @@ def verification(tableau):
     # Vérifie horizontalement si 4 pions sont aligné
     for i in range(b.shape[1] - 3):
         for j in range(b.shape[0]):
-            if b[j][i] == 1 and b[j][i + 1] == 1 and b[j][i + 2] == 1 and b[j][i + 3] == 1:
+            if b[j, i] == 1 and b[j, i + 1] == 1 and b[j, i + 2] == 1 and b[j, i + 3] == 1:
                 return True
     # Vérifie verticalement si 4 pions sont aligner
     for i in range(b.shape[1]):
-        for i in range(b.shape[0] - 3):
-            if b[j][i] == 1 and b[j + 1][i] == 1 and b[j + 2][i] == 1 and b[j + 3][i] == 1:
+        for j in range(b.shape[0] - 3):
+            if b[j, i] == 1 and b[j + 1, i] == 1 and b[j + 2, i] == 1 and b[j + 3, i] == 1:
                 return True
     # Vérifie diagonal
     for i in range(b.shape[1] - 3):
         for j in range(b.shape[0] - 3):
-            if b[j][i] == 1 and b[j + 1][i + 1] == 1 and b[j + 2][i + 2] == 1 and b[j + 3][i + 3] == 1:
+            if b[j, i] == 1 and b[j + 1, i + 1] == 1 and b[j + 2, i + 2] == 1 and b[j + 3, i + 3] == 1:
                 return True
 
 
